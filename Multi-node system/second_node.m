0eb5334d9e532ab3f9f343338846a3c38234eaf9
Suchetaaa <sucheta317@gmail.com>
2019-05-27 17:16:07 +0200

    some more additions

--- Multi-node system/second_node.m
+++ Multi-node system/second_node.m
@@ -1,4 +1,4 @@
-function [arrival_timestamps_all, departure_timestamps_out, ground_indices, largest_time_out] = other_node(departure_timestamps, num_users, lambda_users, mu_node, epsilon_node, largest_time)
+function [arrival_times_in, delay, arrival_timestamps_all, departure_timestamps_out_1, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_node(departure_timestamps, num_users, lambda_users, mu_node, epsilon_node, largest_time, arrival_times_in)
 %     num_users = 10;
 %     lambda_users = abs(randn(1, num_users));
 %     mu_node = 1;
@@ -22,12 +22,15 @@ function [arrival_timestamps_all, departure_timestamps_out, ground_indices, larg
     
     arrival_timestamps_all = sort(event_times_users(:));
     arrival_timestamps_all = arrival_timestamps_all';
+    
     new = [departure_timestamps arrival_timestamps_all];
     arrival_timestamps_all = sort(new(:));
     
     a = arrival_timestamps_all <= largest_time;
     num_useful = numel(a(a>0));
     arrival_timestamps_all = arrival_timestamps_all(1:num_useful, 1)';
+    
+    offset = arrival_timestamps_all(1);
     
     [~, m] = size(departure_timestamps);
     ground_indices = zeros(1, m);
@@ -41,7 +44,7 @@ function [arrival_timestamps_all, departure_timestamps_out, ground_indices, larg
     
     inter_service_times = 1/mu_node*log(1./rand(1,num_useful));
     
-    server_timestamps(1) = 0;
+    server_timestamps(1) = offset;
     departure_timestamps_out(1) = server_timestamps(1) + inter_service_times(1);
     
     for i = 2:num_useful
@@ -53,8 +56,26 @@ function [arrival_timestamps_all, departure_timestamps_out, ground_indices, larg
        departure_timestamps_out(i) = server_timestamps(i) + inter_service_times(i);   
     end
     
+    times = 0:0.5:departure_timestamps_out(num_useful);
+    buffer_lengths = zeros(length(times), 1);
+    for i = 1:length(times)
+        a = arrival_timestamps_all <= times(i);
+        num_arrivals = numel(a(a>0));
+        b = departure_timestamps_out < times(i);
+        num_departures = numel(b(b>0));
+        buffer_lengths(i, 1) = (num_arrivals-num_departures);
+    end
+    
+    waiting_times = (departure_timestamps_out - arrival_timestamps_all(1, 1:num_useful));
+    
+    for i = 1 : m
+        delay = (departure_timestamps_out(ground_indices') - arrival_times_in');
+    end
+        
+        
     random_indices = randperm(num_useful, round((1-epsilon_node)*num_useful));
-    departure_timestamps_out(random_indices) = [];
+    departure_timestamps_out_1 = departure_timestamps_out;
+    departure_timestamps_out_1(random_indices) = [];
    
     l = 0;
     for k = 1 : m
@@ -67,8 +88,13 @@ function [arrival_timestamps_all, departure_timestamps_out, ground_indices, larg
         end
     end
     
-    ground_indices(unwanted_indices) = [];
+    ground_indices(unwanted_indices) = []; 
+    arrival_times_in(unwanted_indices) = [];
     
-    largest_time_out = max(departure_timestamps_out); 
+    for k = 1 : m-l
+        ground_indices_out(1, k) = find(departure_timestamps_out_1 == departure_timestamps_out(1, ground_indices(1, k)));
+    end
+        
+    largest_time_out = max(departure_timestamps_out_1); 
 end
     
