d345b79bcdf0c80b46e7a30089638032dfcfefa1
Suchetaaa <sucheta317@gmail.com>
2019-06-13 12:43:50 +0200

    added periodic poisson

--- Poisson_Periodic/connect.m
+++ Poisson_Periodic/connect.m
@@ -1,30 +1,32 @@
-offset_users = zeros(10, 5);
+num_nodes = 2;
+
 for i = 1:10
 %     epsilon_node(i) = (0.8-0.5).*rand(1, 1) + 0.5;
     epsilon_node(i) = 0.9;
-    offset_users(i, :) = (0.5 - 0).*rand(1, 5) + 0;
 end
+    
+num_users_poisson = 5;
+num_users_periodic = 5;
+num_users = num_users_periodic + num_users_poisson;
 
 %     lambda_users = (0.4-0.1).*rand(10, num_users) + 0.1;
-lambda_users = ones(10, 5);
+lambda_periodic_users = ones(10, num_users_periodic);
+offset_users = (0.5 - 0).*rand(10, num_users_periodic) + 0;
+lambda_poisson_users = ones(10, num_users_poisson);
 higher = 2;
-
-num_nodes = 2;
-    
-num_users = 5;
     
 mu_node = num_users*higher ;
 num_events = 5000;
 num_events_considered = 0.4*(num_users)*num_events;
 
-[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users, lambda_users(1, :), offset_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
+[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users_periodic, num_users_poisson, lambda_periodic_users(1, :), offset_users(1, :), lambda_poisson_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
 
 
 for i = 2:num_nodes
 %     lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
     mu_node = (num_users*i)*higher;
     
-    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users, lambda_users(i, :), offset_users(i, :) ,mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
+    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users_periodic, num_users_poisson, lambda_periodic_users(i, :), offset_users(i, :), lambda_poisson_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
     final_arrival_times = arrival_times_out;
     departure_timestamps = departure_timestamps_out;
     ground_indices = ground_indices_out;
@@ -41,7 +43,6 @@ delay_end2end = departure_timestamps(ground_indices') - arrival_times_out';
 
 [cdf_out, delay_sorted] = cdf(delay_end2end);
 plot(delay_sorted, cdf_out);
-av_age(1) = av_age_func(departure_timestamps(ground_indices'), arrival_times_out');
 title("Nodes=2");
 
 min_delay(1) = min(delay_end2end);
@@ -50,20 +51,22 @@ max_delay(1) = max(delay_end2end);
 
 
 num_nodes = 5;
-num_users = 5;
+num_users_poisson = 5;
+num_users_periodic = 5;
+num_users = num_users_periodic + num_users_poisson;
 
 % lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
 mu_node = num_users*higher ;
 num_events = 5000;
 num_events_considered = 0.4*(num_users)*num_events;
 
-[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users, lambda_users(1, :), offset_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
+[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users_periodic, num_users_poisson, lambda_periodic_users(1, :), offset_users(1, :), lambda_poisson_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
 
 
 for i = 2:num_nodes
 %     lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
     mu_node = (num_users*i)*higher;
-    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users, lambda_users(i, :), offset_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
+    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users_periodic, num_users_poisson, lambda_periodic_users(i, :), offset_users(i, :), lambda_poisson_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
     final_arrival_times = arrival_times_out;
     departure_timestamps = departure_timestamps_out;
     ground_indices = ground_indices_out;
@@ -80,7 +83,6 @@ delay_end2end = departure_timestamps(ground_indices') - arrival_times_out';
 
 [cdf_out, delay_sorted] = cdf(delay_end2end);
 plot(delay_sorted, cdf_out);
-av_age(2) = av_age_func(departure_timestamps(ground_indices'), arrival_times_out');
 title("Nodes=5");
 
 min_delay(2) = min(delay_end2end);
@@ -89,20 +91,22 @@ max_delay(2) = max(delay_end2end);
 
 
 num_nodes = 7;
-num_users = 5;
+num_users_poisson = 5;
+num_users_periodic = 5;
+num_users = num_users_poisson + num_users_periodic;
 
 % lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
 mu_node = num_users*higher;
 num_events = 5000;
 num_events_considered = 0.4*(num_users)*num_events;
 
-[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users, lambda_users(1, :), offset_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
+[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users_periodic, num_users_poisson, lambda_periodic_users(1, :), offset_users(1, :), lambda_poisson_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
 
 
 for i = 2:num_nodes
 %     lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
     mu_node = (num_users*i)*higher;
-    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users, lambda_users(i, :), offset_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
+    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users_periodic, num_users_poisson, lambda_periodic_users(i, :), offset_users(i, :), lambda_poisson_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
     final_arrival_times = arrival_times_out;
     departure_timestamps = departure_timestamps_out;
     ground_indices = ground_indices_out;
@@ -115,9 +119,9 @@ subplot(2, 2, 3)
 delay_end2end = departure_timestamps(ground_indices') - arrival_times_out';
 % [~, m] = size(delay_end2end);
 % plot(1:m, delay_end2end');
+
 [cdf_out, delay_sorted] = cdf(delay_end2end);
 plot(delay_sorted, cdf_out);
-av_age(3) = av_age_func(departure_timestamps(ground_indices'), arrival_times_out');
 title("Nodes=7");
 
 min_delay(3) = min(delay_end2end);
@@ -126,20 +130,22 @@ max_delay(3) = max(delay_end2end);
 
 
 num_nodes = 10;
-num_users = 5;
+num_users_poisson = 5;
+num_users_periodic = 5;
+num_users = num_users_periodic + num_users_poisson;
 
 % lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
 mu_node = num_users*higher;
 num_events = 5000;
 num_events_considered = 0.4*(num_users)*num_events;
 
-[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users, lambda_users(1, :), offset_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
+[ground_indices, final_arrival_times, departure_timestamps, waiting_times, buffer_lengths, largest_time] = first_node(num_users_periodic, num_users_poisson, lambda_periodic_users(1, :), offset_users(1, :), lambda_poisson_users(1, :), mu_node, epsilon_node(1), num_events, num_events_considered);
 
 
 for i = 2:num_nodes
 %     lambda_users = (0.4-0.1).*rand(1, num_users) + 0.1;
     mu_node = (num_users*i)*higher;
-    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users, lambda_users(i, :), offset_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
+    [arrival_times_out, delay, arrival_timestamps_all, departure_timestamps_out, ground_indices_out, largest_time_out, buffer_lengths, waiting_times] = other_nodes(departure_timestamps, num_users_periodic, num_users_poisson, lambda_periodic_users(i, :), offset_users(i, :), lambda_poisson_users(i, :), mu_node, epsilon_node(i), largest_time, final_arrival_times, ground_indices);
     final_arrival_times = arrival_times_out;
     departure_timestamps = departure_timestamps_out;
     ground_indices = ground_indices_out;
@@ -152,9 +158,9 @@ subplot(2, 2, 4)
 delay_end2end = departure_timestamps(ground_indices') - arrival_times_out';
 % [~, m] = size(delay_end2end);
 % plot(1:m, delay_end2end');
+
 [cdf_out, delay_sorted] = cdf(delay_end2end);
 plot(delay_sorted, cdf_out);
-av_age(4) = av_age_func(departure_timestamps(ground_indices'), arrival_times_out');
 title("Nodes = 10");
 
 min_delay(4) = min(delay_end2end);
